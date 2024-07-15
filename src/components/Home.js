38da98d601a222bd8fa0448842416f6d86076774
Tungnvhe <viettungnguyen2010@gmail.com>
2024-07-15 23:37:22 +0700

    Header

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col, Carousel, Offcanvas } from 'react-bootstrap'
+
+
+
+export default function Home() {
+    return (
+        <Container fluid>
+
+
+                <div class="jumbotron">
+                    <h1 class="display-4">Welcome to shop!!</h1>
+                </div>
+        </Container>
+    )
+}
