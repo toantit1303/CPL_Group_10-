7414b51fef3bf243ecedbb129b700de981a9ea7f
Linh <kuroshaki505@gmail.com>
2024-07-18 20:57:39 +0700

    Product list for user and authorization

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './components/Home';
 import Product from './components/Products';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Categories from './components/ExtraMenu';
 
 
 
