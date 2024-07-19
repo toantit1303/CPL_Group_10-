d29d5f82c224e4996b1184f533d6a2c921010466
Trinh Nguyen Ngoc Anh QP3029 <anhtnnhe171240@fpt.edu.vn>
2024-07-19 09:18:43 +0700

    Filter

---
+++ src/components/ExtraMenu.js
@@ -0,0 +1,45 @@
+import { Col, Table } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+export default function Categories({ data = [] }) {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    const isAdmin = user && user.role === 'admin';
+
+    return (
+        <Col>
+            {isAdmin ? (
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><Link to="#">Products</Link></td>
+                        </tr>
+                        <tr>
+                            <td><Link to="#">Account List</Link></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            ) : (
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Categories</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data?.map(c => (
+                            <tr key={c.id}>
+                                <td><Link to={`/product/category/${c.id}`}>{c?.catName}</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </Col>
+    );
+}
